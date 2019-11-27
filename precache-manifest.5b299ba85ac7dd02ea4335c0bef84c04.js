405bb5b74dffca656b6c23c809f72995b227aecf
manea-eugen <bury_30@yahoo.com>
2019-11-27 06:11:32 +0000

    Deploying to gh-pages from master 87f3f2b95a9778018cac3b15de84072f63fbf79f

---
+++ precache-manifest.5b299ba85ac7dd02ea4335c0bef84c04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ecf7e117dcfbb9557cc9d373480777",
+    "url": "https://manea-eugen.github.io/similar-words/index.html"
+  },
+  {
+    "revision": "326fe75bdd38e7244215",
+    "url": "https://manea-eugen.github.io/similar-words/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "61e75cbc2462856d5e78",
+    "url": "https://manea-eugen.github.io/similar-words/static/css/main.2f3eb9d0.chunk.css"
+  },
+  {
+    "revision": "326fe75bdd38e7244215",
+    "url": "https://manea-eugen.github.io/similar-words/static/js/2.a094abcb.chunk.js"
+  },
+  {
+    "revision": "61e75cbc2462856d5e78",
+    "url": "https://manea-eugen.github.io/similar-words/static/js/main.19e2994c.chunk.js"
+  },
+  {
+    "revision": "8361bc33cbf7cdef037f",
+    "url": "https://manea-eugen.github.io/similar-words/static/js/runtime-main.b6a3ec77.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.043839fefb8d0858e8f78ed7be9d74de.js"
+  "https://manea-eugen.github.io/similar-words/precache-manifest.5b299ba85ac7dd02ea4335c0bef84c04.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://manea-eugen.github.io/similar-words/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
