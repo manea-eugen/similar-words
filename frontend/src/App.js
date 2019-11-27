48954ecfaea1dcd5e9588d4630888ae8cadb1fd9
Burycel <bury_30@yahoo.com>
2019-11-27 04:06:33 +0100

    First comits

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <Button type="primary">Test</Button>
+  );
+}
+
+export default App;
